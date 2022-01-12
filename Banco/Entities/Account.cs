bd987b277241629772fc86f16ce5b9c5f9f7381e
Kle3er Alves <klebergithub@gmail.com>
2022-01-12 01:12:12 -0300

    PROJETO DE CONTA BANCARIA COM EXCECOES FINALIZADO

---
+++ Banco/Entities/Account.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Banco.Entities.Exception;
+
+namespace Banco.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(double balance)
+        {
+            Balance = balance;
+        }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainException("A QUANTIA ESTA ACIMA DO LIMITE DO SEU SAQUE!!!");
+            }
+          
+            else if ( Balance < amount)
+            {
+                throw new DomainException("SALDO DA CONTA INSUFICIENTE!!!");
+            }
+
+            else
+            {
+                Balance -= amount;
+            }           
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using Banco.Entities;
+using Banco.Entities.Exception;
+using System.Collections.Generic;
+
+
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());//armazena o numero digitado pelo usuario na variavel "number"
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();//armazena o holder digitado pelo usuario na variavel "holder"
+                Console.Write("Initial Balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                //armazena o saldo digitado pelo usuario na variavel "balance"
+                Console.Write("Withdraw limit: ");
+                double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                //armazena o limite digitado pelo usuario na variavel "limit"
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                //armazena a quantia digitada pelo usuario na variavel "amount"
+                Account account = new Account(number, holder, balance, limit);//passa tudo digitado pelo usuario para a classe Account
+                                                      
+                account.Withdraw(amount);//passa a quantia digitada pelo usuario como parametro para o Metodo "Withdraw"
+                Console.WriteLine("New Balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+                //Exibi na tela o novo Saldo de acordo com o cálculo feito no Metodo "Withdraw"
+            }
+
+            catch (DomainException e)
+            {
+                Console.WriteLine("ERRO NA OPERÇÃO!  " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("ERRO NO FORMATO!  " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERRO INESPERADO!  " + e.Message);
+            }
+        }
+    }
+}
